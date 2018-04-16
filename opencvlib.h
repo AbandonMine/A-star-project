f839661753c467658f62d947891317d8d9bf06cb
Milosz Mazur <36641393+AbandonMine@users.noreply.github.com>
2018-04-16 08:48:07 +0200

    Add files via upload

---
+++ opencvlib.h
@@ -0,0 +1,8 @@
+#ifndef OPENCVLIB_H
+#define OPENCVLIB_H
+#include "opencv2/ml/ml.hpp"
+#include "opencv2/opencv.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/core/core.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#endif // OPENCVLIB_H
